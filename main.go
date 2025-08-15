e70c91db19f1c54902c859b04c982f36adaa4df7
איתי בן חיים <thh123321@gmail.com>
2025-08-15 20:02:02 +0300

    Feat: turned mapper into interface

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	m := MakeMapper()
+	m := MakeMapMapper()
 	fmt.Println(m)
 	m.RegisterRoute("https://gobyexample.com/errors")
 	fmt.Println(m)
--- mapper.go
+++ mapper.go
@@ -6,17 +6,21 @@ import (
 	"errors"
 )
 
-const URL_HASH_LENGTH int = 6
+const urlHashLength = 6
 
-type Mapper struct {
+type Mapper interface {
+	registerRoute(endpoint string) (string, error)
+}
+
+type MapMapper struct {
 	urls map[string]string
 }
 
-func MakeMapper() Mapper {
-	return Mapper{make(map[string]string)}
+func MakeMapMapper() MapMapper {
+	return MapMapper{make(map[string]string)}
 }
 
-func (m *Mapper) RegisterRoute(endpoint string) (string, error) {
+func (m *MapMapper) registerRoute(endpoint string) (string, error) {
 	hash := generateEndpointHash(endpoint)
 
 	if v, exists := m.urls[hash]; exists {
@@ -33,5 +37,5 @@ func (m *Mapper) RegisterRoute(endpoint string) (string, error) {
 
 func generateEndpointHash(endpoint string) string {
 	hash := md5.Sum([]byte(endpoint))
-	return hex.EncodeToString(hash[:])[:URL_HASH_LENGTH]
+	return hex.EncodeToString(hash[:])[:urlHashLength]
 }
